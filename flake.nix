223a3115edeefa7111bde3e0f4d061d7248d54d3
James Sweet <james.sweet@protonmail.com>
2022-10-07 09:41:34 -0400

    feat: initial flake setup

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+
+      in
+      {
+        overlay = nixpkgs.legacyPackages.${system}.callPackage ./pkgs/default.nix {
+          inherit (nixpkgs) lib pkgs;
+        };
+
+        packages = import nixpkgs {
+          inherit system;
+          overlays = [
+            self.overlay.${system}
+          ];
+        };
+      }
+    );
+}
---
+++ pkgs/default.nix
@@ -0,0 +1,4 @@
+{ lib
+, pkgs
+}:
+self: super: rec { }
