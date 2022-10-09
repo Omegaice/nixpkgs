48122e5c4fae7e4af664cc92203d68b3b83f726d
James Sweet <james.sweet@protonmail.com>
2022-10-08 20:00:57 -0400

    feat: added emane software package

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,13 +3,23 @@
   pkgs,
 }: let
   cudaPackageFilter = lib.attrsets.filterAttrs (n: v: builtins.match "cudaPackages_[[:digit:]]+_[[:digit:]]+" n != null);
-
   cudaPackagesExtension = lib.composeManyExtensions [
     (import ./development/libraries/cub/extension.nix)
   ];
 in
-  self: super:
+  overlay-self: overlay-super:
     rec {
-      ospf-mdr = super.callPackage ./applications/networking/ospf-mdr/default.nix {};
+      ospf-mdr = overlay-super.callPackage ./applications/networking/ospf-mdr/default.nix {};
+      emane = overlay-super.callPackage ./applications/networking/emane/default.nix {};
+
+      pythonPackagesExtensions =
+        overlay-super.pythonPackagesExtensions
+        ++ [
+          (python-final: python-prev: {
+            emane = python-prev.toPythonModule (overlay-self.emane.override {
+              python3 = python-prev.python;
+            });
+          })
+        ];
     }
-    // builtins.mapAttrs (name: value: value.overrideScope' cudaPackagesExtension) (cudaPackageFilter super)
+    // builtins.mapAttrs (name: value: value.overrideScope' cudaPackagesExtension) (cudaPackageFilter overlay-super)
