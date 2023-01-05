96206df5f57b41200838af41840ca23dc55faff3
James Sweet <james.sweet@protonmail.com>
2023-01-05 11:45:21 -0500

    feat(vermin): add version 1.5.1

--- pkgs/development/tools/vermin/default.nix
+++ pkgs/development/tools/vermin/default.nix
@@ -3,23 +3,29 @@
   fetchFromGitHub,
   python3Packages,
 }:
-python3Packages.buildPythonApplication rec {
-  pname = "vermin";
-  version = "1.5.1";
+with python3Packages;
+  buildPythonApplication rec {
+    pname = "vermin";
+    version = "1.5.1";
 
-  src = fetchFromGitHub {
-    owner = "netromdk";
-    repo = pname;
-    rev = "v${version}";
-    hash = "sha256-PG6g1FFivkYgI9V/a+yUk7BOPd2ck7ISNLUiatS+/to=";
-  };
+    src = fetchFromGitHub {
+      owner = "netromdk";
+      repo = pname;
+      rev = "v${version}";
+      hash = "sha256-PG6g1FFivkYgI9V/a+yUk7BOPd2ck7ISNLUiatS+/to=";
+    };
 
-  propagatedBuildInputs = with python3Packages; [];
+    propagatedBuildInputs = [];
 
-  meta = with lib; {
-    homepage = "https://github.com/netromdk/vermin";
-    changelog = "https://github.com/netromdk/vermin/releases/tag/v${version}";
-    description = "Concurrently detect the minimum Python versions needed to run code - Vermin 1.6 will end support for running via Python 2.7. Python 3.x is going to be required but detection of 2.x functionality will remain functional.";
-    license = licenses.mit;
-  };
-}
+    checkInputs = [];
+    checkPhase = ''
+      python runtests.py
+    '';
+
+    meta = with lib; {
+      homepage = "https://github.com/netromdk/vermin";
+      changelog = "https://github.com/netromdk/vermin/releases/tag/v${version}";
+      description = "Concurrently detect the minimum Python versions needed to run code - Vermin 1.6 will end support for running via Python 2.7. Python 3.x is going to be required but detection of 2.x functionality will remain functional.";
+      license = licenses.mit;
+    };
+  }
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11,18 +11,14 @@
       salt-lint = pkgs.callPackage ../development/tools/salt-lint/default.nix {};
       steamctl = pkgs.callPackage ../development/tools/steamctl/default.nix {};
       toml-cli = pkgs.callPackage ../development/tools/toml-cli/default.nix {};
+      vermin = pkgs.callPackage ../development/tools/vermin/default.nix {};
     };
 
     packages = {
       salt-lint = final.salt-lint;
       steamctl = final.steamctl;
       toml-cli = final.toml-cli;
-{...}: {
-  flake = {
-    overlays.all = final: prev: {
-      salt-lint = prev.callPackage ../development/tools/salt-lint/default.nix {};
-      steamctl = with prev.python3Packages; callPackage ../development/tools/steamctl/default.nix {};
-      toml-cli = prev.callPackage ../development/tools/toml-cli/default.nix {};
+      vermin = final.vermin;
     };
   };
 }
